e20846e28c76d5873f41eb7d439b8cad75219970
Jaelcio Araujo <jaelcio.contato@yahoo.com.br>
2024-05-27 02:22:06 +0000

    install nodemon

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ import http from "http";
 
 const PORT = 3000;
 
+const rotas = {
+    "/":"Curso de Express API",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rora autores"
+}
+
 const server = http.createServer((req, res) =>{
     res.writeHead(200, {"content-Type": "text/plain"});
-    res.end("Curso de Node.Js");
+    res.end(rotas[req.url]);
 });
 
 server.listen(PORT, () =>{
