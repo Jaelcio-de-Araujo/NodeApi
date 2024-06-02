2c95c03029a451a3a062358c9104180219f49897
Jaelcio Araujo <jaelcio.contato@yahoo.com.br>
2024-06-02 00:03:23 +0000

    add dotenv

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import conectaNaDatabase from "./config/dbconnect.js";
+import livro from "./models/Livro.js";
 
 const conexao = await conectaNaDatabase();
 
@@ -13,29 +14,13 @@ conexao.once("open", () => {
 
 const app = express();
 
-const livros = [
-    {
-        id: 1,
-        titulo: "O Senhor dos Anéis"
-    },
-    {
-        id: 2,
-        titulo: "O Hobbit"
-    }
-]
-
-function buscaLivro(id){
-    return livros.findIndex(livro =>{
-        return livro.id === Number(id);
-    })
-}
-
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.JS");
 });
 
-app.get("/livros", (req, res) => {
-    res.status(200).json(livros);
+app.get("/livros", async(req, res) => {
+    const listaLivros = await livro.find({});
+    res.status(200).json(listaLivros);
 });
 
 
--- src/config/dbconnect.js
+++ src/config/dbconnect.js
@@ -1,10 +1,9 @@
 import mongoose from "mongoose";
 
 async function conectaNaDatabase() {
-    mongoose.connect("mongodb+srv://jaelcioaraujocontato:TnOf1E4QEU6QTQHi@cluster0.ayeinpw.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0");
+    mongoose.connect(process.env.DB_CONNECTION_STRING);
     return mongoose.connection;
 };
 
 export default conectaNaDatabase;
-
 
